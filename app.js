d0344e5971eb759e4d676ef24598cfda2b8dd303
camikbkn <daieluneydami@gmail.com>
2026-03-26 23:46:06 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require("express");
-const cors = require("cors");
-
 const app = express();
 
-app.use(cors({
-    origin: "*",
-    methods: ["GET", "POST", "OPTIONS"],
-    allowedHeaders: ["Content-Type"]
-}));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
 
-app.options("*", cors());
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
+
+    next();
+});
 
 app.use(express.json());
 
