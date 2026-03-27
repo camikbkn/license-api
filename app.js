abf453a76d6ee960ef4808e35755daf34fc96ec5
camikbkn <daieluneydami@gmail.com>
2026-03-26 21:39:23 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,14 +4,27 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const licenses = {
-    BR: ["186.23.164.145"]
+    BR: {
+        active: true,
+        ips: ["186.23.164.145", "181.10.10.10"]
+    },
+    PVP: {
+        active: true,
+        ips: ["123.123.123.123"]
+    }
 };
 
 app.get("/license", (req, res) => {
     const ip = req.query.ip;
     const event = req.query.event;
 
-    if (licenses[event] && licenses[event].includes(ip)) {
+    const data = licenses[event];
+
+    if (!data) return res.send("NO");
+
+    if (!data.active) return res.send("NO");
+
+    if (data.ips.includes(ip)) {
         return res.send("OK");
     }
 
