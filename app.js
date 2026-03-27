04ca559ead4d20a42210946b7191816f689e945f
camikbkn <daieluneydami@gmail.com>
2026-03-26 23:41:37 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express");
-const app = express();
-
 const cors = require("cors");
 
-app.use(cors());
+const app = express();
+
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type"]
+}));
+
+app.options("*", cors());
+
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
